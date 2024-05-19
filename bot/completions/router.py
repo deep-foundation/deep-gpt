da2dd40a81faa3fe181f716db0e4b0509d0b3031
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-05-19 20:58:54 +0300

    Добавит индикатор typing

--- bot/completions/router.py
+++ bot/completions/router.py
@@ -9,6 +9,8 @@ completionsRouter = Router()
 
 @completionsRouter.message()
 async def handle_callback_query(message: Message):
+    await message.bot.send_chat_action(message.chat.id, "typing")
+
     await message.answer(
         completionsService.query_chatgpt(
             message.from_user.id,
