6b47293721338148d8026854f8c9aaf1750a7f2a
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-06-22 21:36:55 +0300

    Уберет соглашение

--- services/agreement_service.py
+++ services/agreement_service.py
@@ -5,14 +5,15 @@ class AgreementService:
     AGREEMENT_STATUS = "agreement-status"
 
     def get_agreement_status(self, user_id: str) -> bool:
-        try:
-            value = data_base[db_key(user_id, self.AGREEMENT_STATUS)].decode('utf-8')
-            if value == "False":
-                return False
-            return True
-        except KeyError:
-            self.set_agreement_status(user_id, False)
-            return False
+        return True
+        # try:
+        #     value = data_base[db_key(user_id, self.AGREEMENT_STATUS)].decode('utf-8')
+        #     if value == "False":
+        #         return False
+        #     return True
+        # except KeyError:
+        #     self.set_agreement_status(user_id, False)
+        #     return False
 
     def set_agreement_status(self, user_id: str, value: bool):
         with data_base.transaction():
