22dcf5d5c872688e85f373c35f98cb59eb11885f
TimaxHack <edtimyr@gmail.com>
2025-01-28 04:48:50 +0300

    дебаг рефералки

--- bot/referral/router.py
+++ bot/referral/router.py
@@ -11,23 +11,32 @@ referralRouter = Router()
 
 @referralRouter.message(TextCommand([referral_command(), referral_command_text()]))
 async def handle_start_referral_generation(message: types.Message):
+    try:
+        bot_info = await message.bot.get_me()
+        user_id = message.from_user.id
+        referral_link = f"https://t.me/{bot_info.username}?start={user_id}"
 
-    bot_info = await message.bot.get_me()
-    user_id = message.from_user.id
-    referral_link = f"https://t.me/{bot_info.username}?start={user_id}"
+        logging.info(f"Пользователь {user_id} запросил реферальную ссылку.")
 
-    logging.info(f"Generated referral link for user {user_id}: {referral_link}")
+        await message.bot.send_chat_action(message.chat.id, "typing")
+        referral = await referralsService.get_referral(user_id)
 
-    await message.bot.send_chat_action(message.chat.id, "typing")
+        if not referral:
+            await message.answer("Реферальная система недоступна.")
+            return
 
-    referral = await referralsService.get_referral(message.from_user.id)
-
-    await message.answer(f"""
+        response_text = f"""
 *15 000*⚡️ за каждого приглашенного пользователя. 
 *+500*⚡️ к ежедневному пополнению баланса за каждого пользователя. 
- 
+
 👩🏻‍💻 Количество рефералов: *{len(referral['children'])}*
 🤑 Ежедневное автопополнение: *{referral['award']}*⚡️
 
 🎉 Ваша реферальная ссылка: `{referral_link}`
-""")
+        """
+        await message.answer(response_text)
+
+    except Exception as e:
+        logging.error(f"Ошибка в реферальной системе: {e}")
+        await message.answer("Произошла ошибка. Попробуйте позже.")
+        
